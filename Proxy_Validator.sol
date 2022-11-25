49d1120ddd29bab468c02ccbf65c3b403913c6df
SolidityX <88942434+solidityx@users.noreply.github.com>
2022-11-25 19:19:21 +0530

    Create Proxy_Validator.sol

---
+++ Proxy_Validator.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+
+interface InterfaceValidator{
+    function distributeBlockReward(address val, uint256 amount) external payable;
+    function updateActiveValidatorSet(address[] memory newSet, uint256 epoch) external;
+}
+
+
+contract Ownership {
+
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(msg.sender);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+
+
+
+contract Proxy_Validator is Ownership {
+
+
+    address public validatorContract;
+
+    modifier onlyMiner() {
+        require(msg.sender == block.coinbase, "Miner only");
+        _;
+    }
+
+    modifier onlyBlockEpoch(uint256 epoch) {
+        require(block.number % epoch == 0, "Block epoch only");
+        _;
+    }
+
+    function setValidatorContract(address validatorContractNew) external onlyOwner returns(bool){
+        require(validatorContractNew != address(0), "Invalid Address");
+        validatorContract = validatorContractNew;
+        return true;
+    }
+
+    function emergencyWithdrawal() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+
+    function distributeBlockReward()
+        external
+        payable
+        onlyMiner
+    {
+        address val = msg.sender;
+        uint256 reward = msg.value;
+        
+        InterfaceValidator(validatorContract).distributeBlockReward{value: msg.value}(val,reward);
+    } 
+
+    function updateActiveValidatorSet(address[] memory newSet, uint256 epoch)
+        public
+        onlyMiner
+        onlyBlockEpoch(epoch)
+    {   
+        InterfaceValidator(validatorContract).updateActiveValidatorSet(newSet,epoch);
+    } 
+
+    
+
+
+
+// Following Proxy contract code taken from openzepelin: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/proxy/Proxy.sol
+
+    /**
+     * @dev Delegates the current call to `implementation`.
+     *
+     * This function does not return to its internal call site, it will return directly to the external caller.
+     */
+    function _delegate(address implementation) internal virtual {
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    
+    /**
+     * @dev Delegates the current call to the address returned by `_implementation()`.
+     *
+     * This function does not return to its internal call site, it will return directly to the external caller.
+     */
+    function _fallback() internal virtual {
+        _delegate(validatorContract);
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if no other
+     * function in the contract matches the call data.
+     */
+    fallback() external payable onlyMiner virtual {
+        _fallback();
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if call data
+     * is empty.
+     */
+    receive() external payable onlyMiner virtual {
+        _fallback();
+    }
+
+
+}
+
+
+
+
